4398f72fbfdb64267329563b2df57bae4b217951
starbong <bs9934@g.yju.ac.kr>
2022-03-11 17:05:06 +0900

    변경

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,7 +3,8 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/join">Join</router-link>
+      <router-link to="/join">Join</router-link>|
+      <router-link to="/bag">Bag</router-link>
     </nav>
     <router-view/>
   </div>
