0331338cf762c3b53ba92f91eaef813d05179845
ass3027 <84160823+ass3027@users.noreply.github.com>
2022-03-17 19:44:56 +0900

    지도 정보 옆에 칸에 입력하기?

---
+++ front/src/components/DateComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="date" v-on:click="select">
+    <p>{{date}}</p>
+    <p>{{location}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DateComponent",
+  props:{
+    date:{
+      type:String,
+      required:true
+    },
+    // location:{
+    //   type:String,
+    //   required:false
+    // }
+  },
+  data() {
+    return{
+      location:''
+    }
+  },
+  methods : {
+    setLocation(location) {
+      this.location = location;
+    },
+    select(e) {
+      //console.log(e.target.id);
+      this.$emit('select',e.target);
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .date{
+    width:100px;
+    height:500px;
+    border: 2px solid;
+
+  }
+</style>
