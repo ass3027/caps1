427cafb81daf3020e7e3dfb008d6c6ac827a58df
SewoonLee <74389789+SewoonLee@users.noreply.github.com>
2022-03-21 19:05:22 +0900

    index.js 라인 정리 CardView.vue 추가 KakaoMap.vue 추가

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,45 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
 import JoinView from '../views/JoinView.vue'
 
 import IndexView from '../views/bag/IndexView'
-import MapView from "@/views/MapView";
+import MapView from '../views/MapView';
+
+import AboutView from '../views/AboutView';
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/join',
-    name: 'join',
-    component:JoinView
-  },
-  {
-    path: '/bag',
-    name: 'bag',
-    component: IndexView
-  },
-  {
-    path: '/map',
-    name: 'map',
-    component:MapView
-  },
-  {
-    path: '/map',
-    name: 'map',
-    component:MapView
-  },
+  { path: '/',      name: 'home',   component: HomeView },
+  { path: '/about', name: 'about',  component: AboutView },
+  { path: '/join',  name: 'join',   component:JoinView },
+  { path: '/bag',   name: 'bag',    component: IndexView },
+  { path: '/map',   name: 'map',    component:MapView },
 ]
 
 const router = new VueRouter({
