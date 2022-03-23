118b692d978111e7a31a8b236d7735507b08a88a
Beopard <93901813+Beopard@users.noreply.github.com>
2022-03-23 12:23:29 +0900

    내준비물 db작업

--- front/src/views/SuppliesView.vue
+++ front/src/views/SuppliesView.vue
@@ -14,17 +14,20 @@
         <hr>
         <ol>
           <li v-for="(item, index) in todoList" :key="index">
-            {{ item.todo }}
-            <button v-on:click="deleteList(item.id)">완료</button>
+            {{ item}}
+            <v-btn v-on:click="makeDone(item.id,item.done)">완료</v-btn>
+            <v-btn fab text small color="green" @click="plusQuantity(item)">+1</v-btn>
+            <v-btn fab text small color="red" @click="minusQuantity(item)">-1</v-btn>
           </li>
         </ol>
         <hr>
         <ol>
           <li v-for="(item, index) in doneList" :key="index">
-            <del>{{ item.todo }}</del>
+            <del>{{ item }}</del>
+            <v-btn v-on:click="makeDone(item.id,item.done)">취소</v-btn>
           </li>
         </ol>
-        <button v-on:click="allDelete">All Delete</button>
+        <v-btn v-on:click="allDelete">완료한 리스트 제거</v-btn>
       </div>
     </div>
   </div>
@@ -39,70 +42,69 @@ export default {
   data() {
     return {
       inputItem: "",
-      todoList: [{done: false, todo: '테스트'}, {done: false, todo: '용'}],
+      todoList: [],
       doneList: [],
       supplies:[]
     }
   },
   mounted() {
-    console.log("hi")
-    axios({
-      method: 'get',
-      url: '/api/getMySupl',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      data: '',
-    })
-        .then((res) => {
-          console.log("첫로딩데이터");
-          console.log(res.data);
-          var todo = [];
-          var done = [];
-
-          res.data.forEach(function (i) {
-            // console.log(i.supl_id.supl_name);
-            // console.log(i.status)
-            if (i.supl_id.supl_id == 0){
-              i.supl_id.supl_name=i.name;
-            }
-            if (i.status == 0) {
-              todo.push({id:i.plan_supl_id,done: false, todo: i.supl_id.supl_name})
-            } else {
-              done.push({id:i.plan_supl_id,done: true, todo: i.supl_id.supl_name})
-            }
-          })
-          // console.log(todo)
-          // console.log(done)
-          this.todoList = todo;
-          this.doneList = done;
-
-        })
-    axios({
-      method: 'get',
-      url: '/api/getSupl',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      data: '',
-    })
-        .then((res) => {
-          console.log("준비물테이블에서가져오기");
-          console.log(res.data);
-          var supplies = []
-
-          res.data.forEach(function (i) {
-            // console.log(i.supl_id.supl_name);
-            // console.log(i.status)
-              supplies.push(i.supl_name)
-          })
-          // console.log(todo)
-          // console.log(done)
-          this.supplies = supplies;
-        })
+    console.log("첫로딩데이터")
+    this.getMyList()
+    this.getList()
   },
   methods: {
+    getList(){
+      axios({
+        method: 'get',
+        url: '/api/getSupl',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: '',
+      })
+          .then((res) => {
+            console.log("준비물테이블에서가져오기");
+            console.log(res.data);
+            var supplies = []
 
+            res.data.forEach(function (i) {
+              supplies.push(i.supl_name)
+            })
+            this.supplies = supplies;
+          })
+
+    },
+    getMyList(){
+      axios({
+        method: 'get',
+        url: '/api/getMySupl',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: '',
+      })
+          .then((res) => {
+            console.log("내리스트가져오기");
+            console.log(res.data);
+            var todo = [];
+            var done = [];
+
+            res.data.forEach(function (i) {
+              if (i.supl_id.supl_id == 0){
+                i.supl_id.supl_name=i.name;
+              }
+              if (i.status == 0) {
+                todo.push({id:i.plan_supl_id,done: false, todo: i.supl_id.supl_name, quantity:i.quantity})
+              } else {
+                done.push({id:i.plan_supl_id,done: true, todo: i.supl_id.supl_name, quantity:i.quantity})
+              }
+            })
+            this.todoList = todo;
+            this.doneList = done;
+
+          })
+
+    },
     inputList(e) {
       e.preventDefault();
       // if (this.inputItem !== "") {
@@ -122,17 +124,80 @@ export default {
         data: JSON.stringify(data2),
       })
           .then(() => {
-            window.location.href = "/supplies";
+            this.getMyList();
+            console.log("삽입완료 다시불러옴")
+            this.inputItem=''
           })
     },
-    deleteList(index) {
-      this.todoList[index].done = "true";
-      this.doneList.push(this.todoList[index]);
-      this.todoList.splice(index, 1)
+    makeDone(id,done) {
+
+      axios({
+        method: 'put',
+        url: '/api/doneSupl',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        data: JSON.stringify({plan_supl_id:id,status:done}),
+      })
+          .then(() => {
+            this.getMyList();
+            console.log("상태수정완료 다시불러옴")
+          })
     },
     allDelete() {
-      this.todoList = [];
-      this.doneList = [];
+      var newDoneList = []
+      this.doneList.forEach(function (i) {
+          newDoneList.push({plan_supl_id:i.id})
+      })
+      console.log(newDoneList)
+      axios({
+        method: 'delete',
+        url: '/api/delSupl',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        data: JSON.stringify(newDoneList),
+      })
+          .then(() => {
+            this.getMyList();
+            console.log("삭제완료 다시불러옴")
+          })
+    },
+    plusQuantity(item) {
+
+      console.log(item)
+      axios({
+        method: 'put',
+        url: '/api/updateQuantity',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        data: JSON.stringify({plan_supl_id:item.id,quantity:item.quantity+1}),
+      })
+          .then(() => {
+            this.getMyList();
+            console.log("quantity수정완료 다시불러옴")
+          })
+    },
+    minusQuantity(item) {
+      console.log(item)
+      if(item.quantity==1)return;
+      axios({
+        method: 'put',
+        url: '/api/updateQuantity',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        data: JSON.stringify({plan_supl_id:item.id,quantity:item.quantity-1}),
+      })
+          .then(() => {
+            this.getMyList();
+            console.log("quantity수정완료 다시불러옴")
+          })
     }
   }
 }
