d1e3f3f1a461c5e3e4335bdfd8a68bc08b4d68ec
hyukeh <80406115+hyukeh@users.noreply.github.com>
2022-04-09 19:20:21 +0900

    .

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -63,11 +63,11 @@
       </div>
       <v-divider class="divider-padding" />
     </div>
-    <img
-      :src="dd"
-      alt="실허어엄"
-      style="width:100px;height:100px"
-    >
+<!--    <img-->
+<!--      :src="dd"-->
+<!--      alt="실허어엄"-->
+<!--      style="width:100px;height:100px"-->
+<!--    >-->
   </v-app>
 </template>
 
@@ -78,7 +78,7 @@ export default {
 
   name: 'HelloWorld',
   data: () => ({
-    dd: `/api/photo/`+"userImage/1648463078479i14085986122.jpg",
+    // dd: `/api/photo/`+"userImage/1648100757821img.jpg",
     menuList:[
       "여행지",
       "여행계획",
