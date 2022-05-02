2e1b4d0f6157a300b900240076a0a00f2f42a3ca
Beopard <93901813+Beopard@users.noreply.github.com>
2022-05-02 10:37:51 +0900

    버그수정

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import VueX from 'vuex';
 import user from './modules/user';
-import hotel from './modules/hotel';
-import createPersistedState from 'vuex-persistedstate';
 import stores from './modules/stores';
 import createPersistedState from 'vuex-persistedstate'
 
@@ -12,7 +10,6 @@ export const store = new VueX.Store({
 
   modules: {
     user,
-    hotel,
     stores
   },
   plugins: [createPersistedState({
