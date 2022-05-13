75c5e0fba8b7783462720c3ecd45b3289c0fd97f
ass3027 <blackfeather227@gmail.com>
2022-05-13 15:36:46 +0900

    planId 버그 ㅋㅋ적용

--- front/src/store/modules/user.js
+++ front/src/store/modules/user.js
@@ -12,11 +12,27 @@ export default {
         },
       updatePlanId(state,newPlanId){
           state.planId = newPlanId
+          console.log(state.planId)
       }
     },
     actions:{
-      updateUserId({commit},newId) {
-        axios.get()
+      setUser({commit},newId) {
+
+        commit('updateUserId',newId)
+        if(newId==='') {
+          commit('updatePlanId',0)
+          return;
+        }
+        axios.get('/api/planner/'+newId)
+          .then( (res)=>{
+            console.log(res.data[0].plan_id)
+            commit('updatePlanId',res.data[0].plan_id)
+          })
+          .catch( (err)=>{
+            console.log(err)
+          })
+
+
       }
     }
 }
