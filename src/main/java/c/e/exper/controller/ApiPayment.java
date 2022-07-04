76f6741233308784124992c68d0548606752fbb7
bon300-03 <blackfeather227@gmail.com>
2022-07-04 16:30:05 +0900

    0704 장소검색 컴포넌트 수정 및 기타 등등?

--- src/main/java/c/e/exper/controller/ApiPayment.java
+++ src/main/java/c/e/exper/controller/ApiPayment.java
@@ -1,7 +1,6 @@
 package c.e.exper.controller;
 
 import c.e.exper.mapper.PaymentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/c/e/exper/mapper/PaymentMapper.java
+++ src/main/java/c/e/exper/mapper/PaymentMapper.java
@@ -10,12 +10,16 @@ import java.util.Map;
 public interface PaymentMapper {
 
     @Select("""
-            SELECT pay.pay_id, t."date", pr.pd_id, pr.pd_name
-            FROM payment pay,product_time t,product pr
+            SELECT pay.pay_id, book.start_date, book.end_date, pr.pd_id, pr.pd_name, book.room_num
+            FROM payment pay, product pr,
+                 (select PAY_ID, max("date") as end_date, min("date") as start_date, PD_ID , room_num
+                  from PRODUCT_TIME
+                  where PAY_ID is not null
+                  group by pay_id, PD_ID,room_num) book
+                              
             WHERE pay.user_id = #{user_id}
-            AND t.pd_id = pr.pd_id
-            AND pay.product_time_num = t.product_time_num
-            AND pay.product_time_num is not null
+            AND book.pd_id = pr.pd_id
+            AND pay.pay_id = book.pay_id
             """)
     List<Map<String,Object>> getProductBook(String user_id);
 }
